dd9d77491f59a8b9697f50c7b4bc1ff867a2cba0
Charles Jolley <charles@sproutit.com>
2008-08-01 19:45:26 -0700

    Add support for addTask

--- clients/todos/controllers/tasks.js
+++ clients/todos/controllers/tasks.js
@@ -14,4 +14,43 @@ require('core');
 Todos.tasksController = SC.ArrayController.create(
 /** @scope Todos.tasksController */ {
 
+  /**
+    Action called when the user presses the Add button.  
+    
+    Creates a new task and adds it to the end of the current array of tasks.
+    Also puts the new task view into edit mode so the user can edit it.
+  */
+  addTask: function() {
+    
+    // Create a new task, with a default title.  
+    // We use newRecord() here because it will add it to the store by default.
+    var task = Todos.Task.newRecord({
+      title: 'Untitled'
+    }) ;
+    
+    // Add the task to the end of the current array.  Note that we use the
+    // SC.Array method, since this is observable.
+    this.pushObject(task) ;
+
+    // Now, we need to get the item view for the new task from the list view.
+    // Since the the task list has not yet had a chance to update with the new
+    // content, we do this the next runloop.
+
+    // Find the list view from the page.
+    var listView = SC.page.getPath('todos.taskListScrollView.taskList') ;
+    var itemView = listView.itemViewForContent(task) ;
+
+    // Begin editing on the found itemView.
+    itemView.beginEditing() ;
+  },
+  
+  /**
+    Action called when the user presses the Delete button.
+    
+    Calls destroy on any selected tasks and removes them from the array.
+  */
+  deleteTask: function() {
+    
+  }
+    
 }) ;
