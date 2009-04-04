2015cb49559e80da512ec2c44dce8e66ea750b34
Charles Jolley <charles@sproutit.com>
2009-04-03 19:07:24 -0700

    Clean out 0.9 source base.  Prepare to start again

--- server/todos-merb/app/models/task.rb
+++
@@ -1,9 +0,0 @@
-class Task
-  include DataMapper::Resource
-
-  property  :id,      Integer,    :serial => true
-  property  :title,   String,     :nullable => false
-  property  :is_done, Boolean,    :default => false 
-  property  :order,   Integer
-
-end
--- server/todos-merb/config/environments/test.rb
+++
@@ -1,5 +0,0 @@
-Merb.logger.info("Loaded TEST Environment...")
-Merb::Config.use { |c|
-  c[:testing] = true
-  c[:exception_details] = true
-}
