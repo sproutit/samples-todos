8a4393830eefcee451532645afd9cce8352b7bfe
Charles Jolley <charles@sproutit.com>
2009-04-04 21:15:26 -0700

    Generate Task model.  Add initial fixtures and schema

---
+++ apps/todos/fixtures/task.js
@@ -0,0 +1,23 @@
+// ==========================================================================
+// Project:   Todos.Task Fixtures
+// Copyright: ©2009 My Company, Inc.
+// ==========================================================================
+/*globals Todos */
+
+sc_require('models/task');
+
+Todos.Task.FIXTURES = [
+
+  { guid: "task-1",
+    title: "Build my first SproutCore app",
+    isDone: false },
+
+  { guid: "task-2",
+    title: "Build a really awesome SproutCore app",
+    isDone: false },
+
+  { guid: "task-3",
+    title: "Next, the world!",
+    isDone: false }
+
+];
---
+++ apps/todos/tests/models/task.js
@@ -0,0 +1,15 @@
+// ==========================================================================
+// Project:   Todos.Task Unit Test
+// Copyright: ©2009 My Company, Inc.
+// ==========================================================================
+/*globals Todos module test ok equals same stop start */
+
+module("Todos.Task");
+
+// TODO: Replace with real unit test for Task
+test("test description", function() {
+  var expected = "test";
+  var result   = "test";
+  equals(result, expected, "test should equal test");
+});
+
