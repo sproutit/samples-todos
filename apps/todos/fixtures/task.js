8fa723d5669ea7d2b0be5aedd6b2d859d582a1a9
Charles Jolley <charles@sproutit.com>
2009-04-04 21:37:57 -0700

    Update fixtures to reflect proper model

--- apps/todos/fixtures/task.js
+++ apps/todos/fixtures/task.js
@@ -8,16 +8,16 @@ sc_require('models/task');
 
 Todos.Task.FIXTURES = [
 
-  { guid: "task-1",
-    title: "Build my first SproutCore app",
-    isDone: false },
+  { "guid": "task-1",
+    "description": "Build my first SproutCore app",
+    "isDone": false },
 
-  { guid: "task-2",
-    title: "Build a really awesome SproutCore app",
-    isDone: false },
+  { "guid": "task-2",
+    "description": "Build a really awesome SproutCore app",
+    "isDone": false },
 
-  { guid: "task-3",
-    title: "Next, the world!",
-    isDone: false }
+  { "guid": "task-3",
+    "description": "Next, the world!",
+    "isDone": false }
 
 ];
