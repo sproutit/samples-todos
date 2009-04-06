ca3ea6ab7bd415fdb336a301deaa153dd78be0d7
Charles Jolley <charles@sproutit.com>
2009-04-05 19:16:14 -0700

    Update main page with initial design

--- apps/todos/english.lproj/main_page.js
+++ apps/todos/english.lproj/main_page.js
@@ -7,15 +7,48 @@
 // This page describes the main user interface for your application.  
 Todos.mainPage = SC.Page.design({
 
-  // The main pane is made visible on screen as soon as your app is loaded.
-  // Add childViews to this pane for views to display immediately on page 
-  // load.
   mainPane: SC.MainPane.design({
-    childViews: 'labelView'.w(),
+    childViews: 'topView middleView bottomView'.w(),
+    classNames: 'main-container',
     
-    labelView: SC.LabelView.design({
-      layout: { centerX: 0, centerY: 0, width: 100, height: 18 },
-      tagName: "h1", value: "Hello World"
+    topView: SC.View.design(SC.Border, {
+      layout: { top: 0, left: 0, right: 0, height: 41 },
+      childViews: 'labelView addButton'.w(),
+      borderStyle: SC.BORDER_BOTTOM,
+      
+      labelView: SC.LabelView.design({
+        layout: { centerY: 0, height: 24, left: 8, width: 200 },
+        controlSize: SC.LARGE_CONTROL_SIZE,
+        fontWeight: SC.BOLD_WEIGHT,
+        value:   'Todos'
+      }),
+      
+      addButton: SC.ButtonView.design({
+        layout: { centerY: 0, height: 21, right: 8, width: 100 },
+       title:  "Add Task"
+      })
+    }),
+    
+    middleView: SC.ScrollView.design({
+      hasHorizontalScroller: NO,
+      layout: { top: 42, bottom: 42, left: 0, right: 0 },
+      backgroundColor: 'white',
+
+      contentView: SC.ListView.design({
+      })
+    }),
+    
+    bottomView: SC.View.design(SC.Border, {
+      layout: { bottom: 0, left: 0, right: 0, height: 41 },
+      childViews: 'summaryView'.w(),
+      borderStyle: SC.BORDER_TOP,
+      
+      summaryView: SC.LabelView.design({
+        layout: { centerY: 0, height: 18, left: 20, right: 20 },
+        textAlign: SC.ALIGN_CENTER,
+        
+        value: "Item Count"
+      })
     })
   })
 
