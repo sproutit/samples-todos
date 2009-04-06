b212c3b163c64671018c0c10f88ac777c4c7f970
Charles Jolley <charles@sproutit.com>
2009-04-05 20:44:29 -0700

    Forgot to save version with controls

--- apps/todos/resources/main_page.js
+++ apps/todos/resources/main_page.js
@@ -12,18 +12,42 @@ Todos.mainPage = SC.Page.design({
     
     topView: SC.View.design(SC.Border, {
       layout: { top: 0, left: 0, right: 0, height: 41 },
-      borderStyle: SC.BORDER_BOTTOM
+      childViews: 'labelView addButton'.w(),
+      borderStyle: SC.BORDER_BOTTOM,
+      
+      labelView: SC.LabelView.design({
+        layout: { centerY: 0, height: 24, left: 8, width: 200 },
+        controlSize: SC.LARGE_CONTROL_SIZE,
+        fontWeight: SC.BOLD_WEIGHT,
+        value:   'Todos'
+      }),
+      
+      addButton: SC.ButtonView.design({
+        layout: { centerY: 0, height: 21, right: 8, width: 100 },
+       title:  "Add Task"
+      })
     }),
     
     middleView: SC.ScrollView.design({
       hasHorizontalScroller: NO,
       layout: { top: 42, bottom: 42, left: 0, right: 0 },
-      backgroundColor: 'white'
+      backgroundColor: 'white',
+
+      contentView: SC.ListView.design({
+      })
     }),
     
     bottomView: SC.View.design(SC.Border, {
       layout: { bottom: 0, left: 0, right: 0, height: 41 },
-      borderStyle: SC.BORDER_TOP
+      childViews: 'summaryView'.w(),
+      borderStyle: SC.BORDER_TOP,
+      
+      summaryView: SC.LabelView.design({
+        layout: { centerY: 0, height: 18, left: 20, right: 20 },
+        textAlign: SC.ALIGN_CENTER,
+        
+        value: "Item Count"
+      })
     })
   })
 
