2f2ddb7cd603ec4c99b0f40e14820aa98393af8e
Charles Jolley <charles@sproutit.com>
2009-04-05 21:04:46 -0700

    Finish changes from step-5

---
+++ apps/todos/controllers/tasks.js
@@ -0,0 +1,18 @@
+// ==========================================================================
+// Project:   Todos.tasksController
+// Copyright: ©2009 My Company, Inc.
+// ==========================================================================
+/*globals Todos */
+
+/** @class
+
+  (Document Your Controller Here)
+
+  @extends SC.ArrayController
+*/
+Todos.tasksController = SC.ArrayController.create(
+/** @scope Todos.tasksController.prototype */ {
+
+  // TODO: Add your own code here.
+
+}) ;
