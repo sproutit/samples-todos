41e10915f922b64e6497e494e48edb792d2cb311
Charles Jolley <charles@sproutit.com>
2009-04-05 20:41:05 -0700

    Final tweaks to UI

--- apps/todos/resources/main_page.js
+++ apps/todos/resources/main_page.js
@@ -9,46 +9,21 @@ Todos.mainPage = SC.Page.design({
 
   mainPane: SC.MainPane.design({
     childViews: 'topView middleView bottomView'.w(),
-    classNames: 'main-container',
     
     topView: SC.View.design(SC.Border, {
       layout: { top: 0, left: 0, right: 0, height: 41 },
-      childViews: 'labelView addButton'.w(),
-      borderStyle: SC.BORDER_BOTTOM,
-      
-      labelView: SC.LabelView.design({
-        layout: { centerY: 0, height: 24, left: 8, width: 200 },
-        controlSize: SC.LARGE_CONTROL_SIZE,
-        fontWeight: SC.BOLD_WEIGHT,
-        value:   'Todos'
-      }),
-      
-      addButton: SC.ButtonView.design({
-        layout: { centerY: 0, height: 21, right: 8, width: 100 },
-       title:  "Add Task"
-      })
+      borderStyle: SC.BORDER_BOTTOM
     }),
     
     middleView: SC.ScrollView.design({
       hasHorizontalScroller: NO,
       layout: { top: 42, bottom: 42, left: 0, right: 0 },
-      backgroundColor: 'white',
-
-      contentView: SC.ListView.design({
-      })
+      backgroundColor: 'white'
     }),
     
     bottomView: SC.View.design(SC.Border, {
       layout: { bottom: 0, left: 0, right: 0, height: 41 },
-      childViews: 'summaryView'.w(),
-      borderStyle: SC.BORDER_TOP,
-      
-      summaryView: SC.LabelView.design({
-        layout: { centerY: 0, height: 18, left: 20, right: 20 },
-        textAlign: SC.ALIGN_CENTER,
-        
-        value: "Item Count"
-      })
+      borderStyle: SC.BORDER_TOP
     })
   })
 
