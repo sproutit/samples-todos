9c3b80d323555a31daf0e8fafee2e4d0bbcc63cc
Martin <martin@martin.local>
2009-04-10 18:00:25 +0200

    Added 20 tasks to illustrate bug

--- apps/todos/fixtures/task.js
+++ apps/todos/fixtures/task.js
@@ -9,15 +9,83 @@ sc_require('models/task');
 Todos.Task.FIXTURES = [
 
   { "guid": "task-1",
-    "description": "Build my first SproutCore app",
+    "description": "This is task 1/20",
     "isDone": false },
 
   { "guid": "task-2",
-    "description": "Build a really awesome SproutCore app",
+    "description": "This is task 2/20",
     "isDone": false },
 
   { "guid": "task-3",
-    "description": "Next, the world!",
-    "isDone": false }
+    "description": "This is task 3/20",
+    "isDone": false },
+
+  { "guid": "task-4",
+    "description": "This is task 4/20",
+    "isDone": false },
+
+  { "guid": "task-5",
+    "description": "This is task 5/20",
+    "isDone": false },
+
+  { "guid": "task-6",
+    "description": "This is task 6/20",
+    "isDone": false },
+
+  { "guid": "task-7",
+    "description": "This is task 7/20",
+    "isDone": false },
+
+  { "guid": "task-8",
+    "description": "This is task 8/20",
+    "isDone": false },
+
+  { "guid": "task-9",
+    "description": "This is task 9/20",
+    "isDone": false },
+
+  { "guid": "task-10",
+    "description": "This is task 10/20",
+    "isDone": false },
+
+  { "guid": "task-11",
+    "description": "This is task 11/20",
+    "isDone": false },
+
+  { "guid": "task-12",
+    "description": "This is task 12/20",
+    "isDone": false },
+
+  { "guid": "task-13",
+    "description": "This is task 13/20",
+    "isDone": false },
+
+  { "guid": "task-14",
+    "description": "This is task 14/20",
+    "isDone": false },
+
+  { "guid": "task-15",
+    "description": "This is task 15/20",
+    "isDone": false },
+
+  { "guid": "task-16",
+    "description": "This is task 16/20",
+    "isDone": false },
+
+  { "guid": "task-17",
+    "description": "This is task 17/20",
+    "isDone": false },
+
+  { "guid": "task-18",
+    "description": "This is task 18/20",
+    "isDone": false },
+
+  { "guid": "task-19",
+    "description": "This is task 19/20",
+    "isDone": false },
+
+  { "guid": "task-20",
+    "description": "This is task 20/20",
+    "isDone": false },
 
 ];
